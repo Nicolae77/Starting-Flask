20c9f980f9ec5f4d6320059f3a77704277be5f6d
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-09-29 12:16:18 +0100

    Improving server.py file

--- server.py
+++ server.py
@@ -3,13 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/index.html")
 def my_home():
     return render_template('index.html')
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     return render_template('about.html')
 
-@app.route("/blog")
-def blog():
-    return "This is my blog"
+@app.route("/works.html")
+def works():
+    return render_template('works.html')
+
+@app.route("/work.html")
+def work():
+    return render_template('work.html')
+
+@app.route("/contact.html")
+def contact():
+    return render_template('contact.html')
