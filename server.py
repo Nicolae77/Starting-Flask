e6ce3749b72037bb997f134ac511a29fb0daa1c0
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-09-30 11:41:13 +0100

    Set up database

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 
+import email
+from email import message
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
@@ -10,11 +12,18 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject =data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('/thankyou.html')
     else:
         return 'something went wrong'
