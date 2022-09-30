e111b50546fd8670c2c6fe056c8fc0a6079f3c42
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-09-30 13:10:52 +0100

    Index.html modified

--- server.py
+++ server.py
@@ -22,21 +22,18 @@ def write_to_file(data):
         file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
-    with open('database.csv', mode='a', newline='') as database2:
+    with open('database.csv', mode='a') as database2:
         email = data['email']
         subject =data['subject']
         message = data['message']
-        csv_writer = csv.writer(database2, delimiter=',', quotechar='|',  quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email,subject,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        try:
-            data = request.form.to_dict()
-            write_to_csv(data)
-            return redirect('/thankyou.html')
-        except:
-            return 'Did not save to database'
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
     else:
         return 'something went wrong'
