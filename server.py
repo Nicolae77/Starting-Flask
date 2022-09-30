0af456f7b810b6cfd8d022fdee48a75946e84181
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-09-30 11:01:28 +0100

    Server.py file modified

--- server.py
+++ server.py
@@ -1,36 +1,20 @@
 
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template('index.html')
-
-@app.route("/index.html")
 def my_home():
     return render_template('index.html')
 
-@app.route("/about.html")
-def about():
-    return render_template('about.html')
-
-@app.route("/works.html")
-def works():
-    return render_template('works.html')
-
-@app.route("/work.html")
-def work():
-    return render_template('work.html')
-
-@app.route("/contact.html")
-def contact():
-    return render_template('contact.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
         print(data)
-        return redirect('/thankyou.html')
+        return 'form submited'
     else:
         return 'something went wrong'
